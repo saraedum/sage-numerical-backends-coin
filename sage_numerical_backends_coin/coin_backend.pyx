64555e437da07940235fd796671c347119025c1a
Julian Rüth <julian.rueth@fsfe.org>
2020-11-26 16:08:08 +0100

    Fix copy & paste mistakes

--- sage_numerical_backends_coin/coin_backend.pyx
+++ sage_numerical_backends_coin/coin_backend.pyx
@@ -107,9 +107,9 @@ cdef class CoinBackend(GenericBackend):
 
         - ``binary`` - ``True`` if the variable is binary (default: ``False``).
 
-        - ``continuous`` - ``True`` if the variable is binary (default: ``True``).
+        - ``continuous`` - ``True`` if the variable is continuous (default: ``True``).
 
-        - ``integer`` - ``True`` if the variable is binary (default: ``False``).
+        - ``integer`` - ``True`` if the variable is integer (default: ``False``).
 
         - ``obj`` - (optional) coefficient of this variable in the objective function (default: 0.0)
 
